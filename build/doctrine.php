eefa594f3749e35fb7ae1e3471a438639c99a47c
Thomas Boileau <t-boileau@email.com>
2022-01-14 01:52:20 +0100

    Tools (#10)
    * build: install and configure static analytics tools
    
    * build: install and configure phpunit
    
    * build: delete docker files

---
+++ build/doctrine.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Kernel;
+
+require __DIR__.'/bootstrap.php';
+
+$kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
+$kernel->boot();
+
+return $kernel->getContainer()->get('doctrine')->getManager();
