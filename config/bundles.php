8f101475efaa66ca6c82891b984dcb17efc5c326
TBoileau <t-boileau@email.com>
2022-01-19 10:28:03 +0100

    build: install twig bundle

--- config/bundles.php
+++ config/bundles.php
@@ -7,4 +7,6 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
 ];
