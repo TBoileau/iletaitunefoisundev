4b9a5601f74143e6ed0a4cff4fd0c9a8bbe91f7a
TBoileau <t-boileau@email.com>
2022-01-19 09:53:56 +0100

    feat (security): implement User entity (#14)

---
+++ src/Repository/UserRepository.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Repository;
+
+use App\Entity\User;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+final class UserRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, User::class);
+    }
+}
