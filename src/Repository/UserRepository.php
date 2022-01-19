81d6e4f65878222bc00e04a523bfd85e6ddfc973
TBoileau <t-boileau@email.com>
2022-01-19 11:10:14 +0100

    refactor: implement UserLoaderInterface

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -7,16 +7,23 @@ namespace App\Repository;
 use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bridge\Doctrine\Security\User\UserLoaderInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @template T
  *
  * @template-extends ServiceEntityRepository<User>
  */
-final class UserRepository extends ServiceEntityRepository
+final class UserRepository extends ServiceEntityRepository implements UserLoaderInterface
 {
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, User::class);
+    }
+
+    public function loadUserByIdentifier(string $identifier): ?UserInterface
+    {
+        return $this->findOneBy(['email' => $identifier]);
     }
 }
