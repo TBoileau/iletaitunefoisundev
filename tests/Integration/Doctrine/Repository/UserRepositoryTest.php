b47ed6a37036ace67e13d147071302361bda8dc3
TBoileau <t-boileau@email.com>
2022-01-19 11:09:10 +0100

    test (security): add integration test
    * UserRepository : loadUserByIdentifier

---
+++ tests/Integration/Doctrine/Repository/UserRepositoryTest.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Integration\Doctrine\Repository;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+final class UserRepositoryTest extends KernelTestCase
+{
+    /**
+     * @test
+     */
+    public function loadUserByIdentifierShouldReturnUser(): void
+    {
+        self::bootKernel();
+
+        /** @var UserRepository $userRepository */
+        $userRepository = self::getContainer()->get(UserRepository::class);
+
+        $user = $userRepository->loadUserByIdentifier('user+1@email.com');
+
+        self::assertNotNull($user);
+        self::assertInstanceOf(User::class, $user);
+    }
+}
