c203ba61132e34b9bd1940c7f277b5b3c835da4b
TBoileau <t-boileau@email.com>
2022-01-19 09:42:17 +0100

    build: install security bundle

--- config/bundles.php
+++ config/bundles.php
@@ -6,4 +6,5 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
 ];
