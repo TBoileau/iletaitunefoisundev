19e28c4fdc221321fb0874693693dfe88da5a063
Thomas Boileau <t-boileau@email.com>
2022-01-20 19:05:10 +0100

    Adventure (#50)
    * feat (adventure): implement Level entity (#47)
    
    * feat (adventure): implement Map entity (#46)
    
    * style: lint php files
    
    * feat (adventure): create map and level fixtures (#48,#49)

---
+++ src/Infrastructure/DataFixtures/MapFixtures.php
@@ -0,0 +1,84 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Infrastructure\DataFixtures;
+
+use App\Domain\Adventure\Entity\Level;
+use App\Domain\Adventure\Entity\Map;
+use App\Domain\Course\Entity\Course;
+use App\Domain\Shared\Uuid\UuidGeneratorInterface;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+
+final class MapFixtures extends Fixture implements DependentFixtureInterface
+{
+    public function __construct(private UuidGeneratorInterface $uuidGenerator)
+    {
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        /** @var array<int, Course> $courses */
+        $courses = $manager->getRepository(Course::class)->findAll();
+
+        $previousMap = null;
+
+        for ($i = 1; $i <= 5; ++$i) {
+            $map = $this->createMap($i, $previousMap);
+
+            $manager->persist($map);
+
+            $mapCourses = array_slice($courses, ($i - 1) * 10, 10);
+
+            $previousLevel = null;
+
+            foreach ($mapCourses as $order => $course) {
+                $level = $this->createLevel($order + 1, $course, $map, $previousLevel);
+
+                if (0 === $order) {
+                    $map->setStart($level);
+                }
+
+                $manager->persist($level);
+
+                $previousLevel = $level;
+            }
+
+            $previousMap = $map;
+        }
+
+        $manager->flush();
+    }
+
+    /**
+     * @return array<array-key, class-string<Fixture>>
+     */
+    public function getDependencies(): array
+    {
+        return [CourseFixtures::class];
+    }
+
+    private function createMap(int $index, ?Map $previous): Map
+    {
+        $map = new Map();
+        $map->setId($this->uuidGenerator->generate());
+        $map->setPrevious($previous);
+        $map->setName(sprintf('Map %d', $index));
+
+        return $map;
+    }
+
+    private function createLevel(int $order, Course $course, Map $map, ?Level $previous): Level
+    {
+        $level = new Level();
+        $level->setId($this->uuidGenerator->generate());
+        $level->setPrevious($previous);
+        $level->setMap($map);
+        $level->setCourse($course);
+        $level->setOrder($order);
+
+        return $level;
+    }
+}
--- tests/Component/RepositoryTest.php
+++ tests/Component/RepositoryTest.php
@@ -4,10 +4,14 @@ declare(strict_types=1);
 
 namespace App\Tests\Component;
 
+use App\Domain\Adventure\Entity\Level;
+use App\Domain\Adventure\Entity\Map;
 use App\Domain\Course\Entity\Course;
 use App\Domain\Node\Entity\Node;
 use App\Domain\Security\Entity\User;
 use App\Infrastructure\Repository\CourseRepository;
+use App\Infrastructure\Repository\LevelRepository;
+use App\Infrastructure\Repository\MapRepository;
 use App\Infrastructure\Repository\NodeRepository;
 use App\Infrastructure\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
@@ -44,5 +48,7 @@ final class RepositoryTest extends KernelTestCase
         yield 'user entity' => [User::class, UserRepository::class]; /* @phpstan-ignore-line */
         yield 'node entity' => [Node::class, NodeRepository::class]; /* @phpstan-ignore-line */
         yield 'course entity' => [Course::class, CourseRepository::class]; /* @phpstan-ignore-line */
+        yield 'map entity' => [Map::class, MapRepository::class]; /* @phpstan-ignore-line */
+        yield 'level entity' => [Level::class, LevelRepository::class]; /* @phpstan-ignore-line */
     }
 }
