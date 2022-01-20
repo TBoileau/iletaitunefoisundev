222155f764bc83a7ddcacd3c7c3b25b540e5779e
TBoileau <t-boileau@email.com>
2022-01-20 13:21:52 +0100

    feat (node): implement Step entity (#40)

---
+++ src/Domain/Node/Entity/Step.php
@@ -0,0 +1,109 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Domain\Node\Entity;
+
+use App\Domain\Security\Entity\User;
+use DateTimeImmutable;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping\Column;
+use Doctrine\ORM\Mapping\Entity;
+use Doctrine\ORM\Mapping\GeneratedValue;
+use Doctrine\ORM\Mapping\Id;
+use Doctrine\ORM\Mapping\JoinColumn;
+use Doctrine\ORM\Mapping\ManyToOne;
+
+#[Entity]
+class Step
+{
+    #[Id]
+    #[GeneratedValue]
+    #[Column(type: Types::INTEGER)]
+    private ?int $id = null;
+
+    #[ManyToOne(targetEntity: Node::class)]
+    #[JoinColumn(nullable: false)]
+    private Node $node;
+
+    #[ManyToOne(targetEntity: User::class)]
+    #[JoinColumn(nullable: false)]
+    private User $user;
+
+    #[Column(type: Types::DATETIME_IMMUTABLE)]
+    private DateTimeImmutable $startedAt;
+
+    #[Column(type: Types::DATETIME_IMMUTABLE, nullable: true)]
+    private ?DateTimeImmutable $finishedAt = null;
+
+    #[Column(type: Types::INTEGER, nullable: true)]
+    private ?int $grade = null;
+
+    #[Column(type: Types::TEXT, nullable: true)]
+    private ?string $comment = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNode(): Node
+    {
+        return $this->node;
+    }
+
+    public function setNode(Node $node): void
+    {
+        $this->node = $node;
+    }
+
+    public function getUser(): User
+    {
+        return $this->user;
+    }
+
+    public function setUser(User $user): void
+    {
+        $this->user = $user;
+    }
+
+    public function getStartedAt(): DateTimeImmutable
+    {
+        return $this->startedAt;
+    }
+
+    public function setStartedAt(DateTimeImmutable $startedAt): void
+    {
+        $this->startedAt = $startedAt;
+    }
+
+    public function getFinishedAt(): ?DateTimeImmutable
+    {
+        return $this->finishedAt;
+    }
+
+    public function setFinishedAt(?DateTimeImmutable $finishedAt): void
+    {
+        $this->finishedAt = $finishedAt;
+    }
+
+    public function getGrade(): ?int
+    {
+        return $this->grade;
+    }
+
+    public function setGrade(?int $grade): void
+    {
+        $this->grade = $grade;
+    }
+
+    public function getComment(): ?string
+    {
+        return $this->comment;
+    }
+
+    public function setComment(?string $comment): void
+    {
+        $this->comment = $comment;
+    }
+}
