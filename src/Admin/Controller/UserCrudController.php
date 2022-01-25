ed0e6855e3c5d60304a97cc32cd2efc2fa46e0af
TBoileau <t-boileau@email.com>
2022-01-25 10:16:00 +0100

    feat (admin): manage users (#60)

---
+++ src/Admin/Controller/UserCrudController.php
@@ -0,0 +1,86 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Admin\Controller;
+
+use App\Admin\EasyAdmin\Field\YoutubeField;
+use App\Core\Uid\UlidGeneratorInterface;
+use App\Node\Entity\Course;
+use App\Security\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
+use EasyCorp\Bundle\EasyAdminBundle\Contracts\Field\FieldInterface;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\SlugField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Filter\TextFilter;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Uid\Ulid;
+
+final class UserCrudController extends AbstractCrudController
+{
+    public function __construct(
+        private UlidGeneratorInterface $ulidGenerator,
+        private UserPasswordHasherInterface $userPasswordHasher
+    ) {
+    }
+
+    public static function getEntityFqcn(): string
+    {
+        return User::class;
+    }
+
+    public function configureFilters(Filters $filters): Filters
+    {
+        return $filters->add(TextFilter::new('email', 'Email'));
+    }
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInSingular('Utilisateur')
+            ->setEntityLabelInPlural('Utilisateurs')
+            ->setDefaultSort(['email' => 'ASC']);
+    }
+
+    public function configureActions(Actions $actions): Actions
+    {
+        return $actions->add(Crud::PAGE_INDEX, Action::DETAIL);
+    }
+
+    /**
+     * @return iterable<FieldInterface>
+     */
+    public function configureFields(string $pageName): iterable
+    {
+        yield TextField::new('email', 'Email');
+        yield TextField::new('password', 'Mot de passe')->onlyWhenCreating();
+    }
+
+    public function createEntity(string $entityFqcn): User
+    {
+        $user = new User();
+        $user->setId($this->ulidGenerator->generate());
+        return $user;
+    }
+
+    /**
+     * @param User $entityInstance
+     */
+    public function persistEntity(EntityManagerInterface $entityManager, $entityInstance): void
+    {
+        $entityInstance->setPassword(
+            $this->userPasswordHasher->hashPassword(
+                $entityInstance,
+                $entityInstance->getPassword()
+            )
+        );
+        parent::persistEntity($entityManager, $entityInstance);
+    }
+}
---
+++ tests/Functional/Admin/UserTest.php
@@ -0,0 +1,184 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Functional\Admin;
+
+use App\Admin\Controller\UserCrudController;
+use App\Node\Entity\Course;
+use App\Security\Entity\Administrator;
+use App\Security\Entity\User;
+use App\Security\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Uid\Ulid;
+
+final class UserTest extends WebTestCase
+{
+    /**
+     * @test
+     */
+    public function shouldShowListOfUsers(): void
+    {
+        $client = self::createClient();
+
+        /** @var EntityManagerInterface $entityManager */
+        $entityManager = $client->getContainer()->get(EntityManagerInterface::class);
+
+        /** @var Administrator $admin */
+        $admin = $entityManager->getRepository(Administrator::class)->findOneBy(['email' => 'admin+1@email.com']);
+
+        $client->loginUser($admin, 'admin');
+
+        /** @var AdminUrlGenerator $adminUrlGenerator */
+        $adminUrlGenerator = $client->getContainer()->get(AdminUrlGenerator::class);
+
+        $client->request(
+            'GET',
+            $adminUrlGenerator
+                ->setController(UserCrudController::class)
+                ->setAction(Action::INDEX)
+                ->generateUrl()
+        );
+
+        self::assertResponseIsSuccessful();
+    }
+
+    /**
+     * @test
+     */
+    public function shouldShowDetailOfUser(): void
+    {
+        $client = self::createClient();
+
+        /** @var EntityManagerInterface $entityManager */
+        $entityManager = $client->getContainer()->get(EntityManagerInterface::class);
+
+        /** @var Administrator $admin */
+        $admin = $entityManager->getRepository(Administrator::class)->findOneBy(['email' => 'admin+1@email.com']);
+
+        $client->loginUser($admin, 'admin');
+
+        /** @var AdminUrlGenerator $adminUrlGenerator */
+        $adminUrlGenerator = $client->getContainer()->get(AdminUrlGenerator::class);
+
+        /** @var User $user */
+        $user = $entityManager->getRepository(User::class)->findOneBy(['email' => 'user+1@email.com']);
+
+        $client->request(
+            'GET',
+            $adminUrlGenerator
+                ->setController(UserCrudController::class)
+                ->setAction(Action::DETAIL)
+                ->setEntityId($user->getId())
+                ->generateUrl()
+        );
+
+        self::assertResponseIsSuccessful();
+    }
+
+    /**
+     * @test
+     */
+    public function shouldCreateUser(): void
+    {
+        $client = self::createClient();
+
+        /** @var EntityManagerInterface $entityManager */
+        $entityManager = $client->getContainer()->get(EntityManagerInterface::class);
+
+        /** @var Administrator $admin */
+        $admin = $entityManager->getRepository(Administrator::class)->findOneBy(['email' => 'admin+1@email.com']);
+
+        /** @var Course $randomCourse */
+        $randomCourse = $entityManager->getRepository(Course::class)->findOneBy([]);
+
+        $client->loginUser($admin, 'admin');
+
+        /** @var AdminUrlGenerator $adminUrlGenerator */
+        $adminUrlGenerator = $client->getContainer()->get(AdminUrlGenerator::class);
+
+        $client->request(
+            'GET',
+            $adminUrlGenerator
+                ->setController(UserCrudController::class)
+                ->setAction(Action::NEW)
+                ->generateUrl()
+        );
+
+        self::assertResponseIsSuccessful();
+
+        $client->submitForm('Créer', [
+            'User[email]' => 'user+6@email.com',
+            'User[password]' => 'Password123!',
+        ]);
+
+        self::assertResponseStatusCodeSame(Response::HTTP_FOUND);
+
+        /** @var UserRepository<User> $userRepository */
+        $userRepository = $client->getContainer()->get(UserRepository::class);
+
+        $user = $userRepository->findOneBy(['email' => 'user+6@email.com']);
+
+        /** @var UserPasswordHasherInterface $userPasswordHasher */
+        $userPasswordHasher = $client->getContainer()->get(UserPasswordHasherInterface::class);
+
+        self::assertNotNull($user);
+        self::assertInstanceOf(User::class, $user);
+        self::assertSame('user+6@email.com', $user->getEmail());
+        self::assertTrue($userPasswordHasher->isPasswordValid($user, 'Password123!'));
+        self::assertTrue(Ulid::isValid((string) $user->getId()));
+    }
+
+    /**
+     * @test
+     */
+    public function shouldUpdateUser(): void
+    {
+        $client = self::createClient();
+
+        /** @var EntityManagerInterface $entityManager */
+        $entityManager = $client->getContainer()->get(EntityManagerInterface::class);
+
+        /** @var Administrator $admin */
+        $admin = $entityManager->getRepository(Administrator::class)->findOneBy(['email' => 'admin+1@email.com']);
+
+        /** @var User $user */
+        $user = $entityManager->getRepository(User::class)->findOneBy(['email' => 'user+1@email.com']);
+
+        $client->loginUser($admin, 'admin');
+
+        /** @var AdminUrlGenerator $adminUrlGenerator */
+        $adminUrlGenerator = $client->getContainer()->get(AdminUrlGenerator::class);
+
+        $client->request(
+            'GET',
+            $adminUrlGenerator
+                ->setController(UserCrudController::class)
+                ->setAction(Action::EDIT)
+                ->setEntityId((string) $user->getId())
+                ->generateUrl()
+        );
+
+        self::assertResponseIsSuccessful();
+
+        $client->submitForm('Sauvegarder les modifications', [
+            'User[email]' => 'user+6@email.com',
+        ]);
+
+        self::assertResponseStatusCodeSame(Response::HTTP_FOUND);
+
+        /** @var UserRepository<User> $userRepository */
+        $userRepository = $client->getContainer()->get(UserRepository::class);
+
+        $user = $userRepository->findOneBy(['email' => 'user+6@email.com']);
+
+        self::assertNotNull($user);
+        self::assertInstanceOf(User::class, $user);
+        self::assertSame('user+6@email.com', $user->getEmail());
+    }
+}
