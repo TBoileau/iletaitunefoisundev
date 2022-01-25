4e0b2349c1d3cfbdcf913ba2aa5e384a97128865
Thomas Boileau <t-boileau@email.com>
2022-01-25 10:22:27 +0100

    Manage users (#61)
    * feat (admin): manage users (#60)
    
    * feat (admin): add link to user crud
    
    * fix (admin): siblings template
    
    * style: lint php files
    
    * fix: phpstan errors

---
+++ src/Admin/Controller/UserCrudController.php
@@ -0,0 +1,80 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Admin\Controller;
+
+use App\Core\Uid\UlidGeneratorInterface;
+use App\Security\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
+use EasyCorp\Bundle\EasyAdminBundle\Contracts\Field\FieldInterface;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Filter\TextFilter;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+final class UserCrudController extends AbstractCrudController
+{
+    public function __construct(
+        private UlidGeneratorInterface $ulidGenerator,
+        private UserPasswordHasherInterface $userPasswordHasher
+    ) {
+    }
+
+    public static function getEntityFqcn(): string
+    {
+        return User::class;
+    }
+
+    public function configureFilters(Filters $filters): Filters
+    {
+        return $filters->add(TextFilter::new('email', 'Email'));
+    }
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInSingular('Utilisateur')
+            ->setEntityLabelInPlural('Utilisateurs')
+            ->setDefaultSort(['email' => 'ASC']);
+    }
+
+    public function configureActions(Actions $actions): Actions
+    {
+        return $actions->add(Crud::PAGE_INDEX, Action::DETAIL);
+    }
+
+    /**
+     * @return iterable<FieldInterface>
+     */
+    public function configureFields(string $pageName): iterable
+    {
+        yield TextField::new('email', 'Email');
+        yield TextField::new('password', 'Mot de passe')->onlyWhenCreating();
+    }
+
+    public function createEntity(string $entityFqcn): User
+    {
+        $user = new User();
+        $user->setId($this->ulidGenerator->generate());
+
+        return $user;
+    }
+
+    public function persistEntity(EntityManagerInterface $entityManager, mixed $entityInstance): void
+    {
+        if ($entityInstance instanceof User && $entityInstance->getPassword() !== null) {
+            $entityInstance->setPassword(
+                $this->userPasswordHasher->hashPassword(
+                    $entityInstance,
+                    $entityInstance->getPassword()
+                )
+            );
+        }
+        parent::persistEntity($entityManager, $entityInstance);
+    }
+}
