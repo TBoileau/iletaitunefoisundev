c830b0292dfc7a1ee0937b7490ec03852ea40fcc
TBoileau <t-boileau@email.com>
2022-01-26 15:20:17 +0100

    feat (adventure): create region fixtures (#89)

---
+++ src/Adventure/DataFixtures/RegionFixtures.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Adventure\DataFixtures;
+
+use App\Adventure\Entity\Region;
+use App\Adventure\Entity\Continent;
+use App\Core\Uid\UlidGeneratorInterface;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+
+final class RegionFixtures extends Fixture implements DependentFixtureInterface
+{
+    public function __construct(private UlidGeneratorInterface $ulidGenerator)
+    {
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        /** @var array<array-key, Continent> $continents */
+        $continents = $manager->getRepository(Continent::class)->findAll();
+
+        foreach ($continents as $continent) {
+            for ($i = 1; $i <= 5; ++$i) {
+                $region = new Region();
+                $region->setId($this->ulidGenerator->generate());
+                $region->setName(sprintf('Region %d', $i));
+                $region->setContinent($continent);
+                $manager->persist($region);
+            }
+        }
+
+        $manager->flush();
+    }
+
+    /**
+     * @return array<array-key, class-string<Fixture>>
+     */
+    public function getDependencies(): array
+    {
+        return [ContinentFixtures::class];
+    }
+}
