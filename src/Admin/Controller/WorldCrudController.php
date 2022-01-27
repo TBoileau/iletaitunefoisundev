6a0d4cfb6344535cd92d728f10f524cdcbdf30af
TBoileau <t-boileau@email.com>
2022-01-27 11:34:42 +0100

    feat (admin): manage worlds (#109)

---
+++ src/Admin/Controller/WorldCrudController.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Admin\Controller;
+
+use App\Adventure\Entity\World;
+use App\Core\Uid\UlidGeneratorInterface;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
+use EasyCorp\Bundle\EasyAdminBundle\Contracts\Field\FieldInterface;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Filter\TextFilter;
+
+final class WorldCrudController extends AbstractCrudController
+{
+    public function __construct(private UlidGeneratorInterface $ulidGenerator)
+    {
+    }
+
+    public static function getEntityFqcn(): string
+    {
+        return World::class;
+    }
+
+    public function configureFilters(Filters $filters): Filters
+    {
+        return $filters->add(TextFilter::new('name', 'Nom'));
+    }
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInSingular('Monde')
+            ->setEntityLabelInPlural('Mondes')
+            ->setDefaultSort(['name' => 'ASC']);
+    }
+
+    public function configureActions(Actions $actions): Actions
+    {
+        return $actions->add(Crud::PAGE_INDEX, Action::DETAIL);
+    }
+
+    /**
+     * @return iterable<FieldInterface>
+     */
+    public function configureFields(string $pageName): iterable
+    {
+        yield TextField::new('name', 'Nom');
+        yield AssociationField::new('continents', 'Continents')
+            ->setTemplatePath('admin/field/continents.html.twig')
+            ->hideOnForm();
+    }
+
+    public function createEntity(string $entityFqcn): World
+    {
+        $world = new World();
+        $world->setId($this->ulidGenerator->generate());
+
+        return $world;
+    }
+}
