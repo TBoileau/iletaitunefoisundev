301cb6e8c3b7e9fcaa29645a0a0ac97213d8f59d
TBoileau <t-boileau@email.com>
2022-01-28 10:10:00 +0100

    build: install gesdinet/jwt-refresh-token-bundle

--- server/config/bundles.php
+++ server/config/bundles.php
@@ -12,4 +12,5 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    Gesdinet\JWTRefreshTokenBundle\GesdinetJWTRefreshTokenBundle::class => ['all' => true],
 ];
