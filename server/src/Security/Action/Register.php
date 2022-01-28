7783fbd5f1cffb7c29f160a6ce78c06bce0a7735
Thomas Boileau <t-boileau@email.com>
2022-01-28 11:19:24 +0100

    Api doc (#133)
    * build: install nelmio/api-doc-bundle
    
    * feat (security): add api doc for registrer (#119)
    
    * feat (security): delete unnecessary templates (#123)

--- server/src/Security/Action/Register.php
+++ server/src/Security/Action/Register.php
@@ -6,6 +6,8 @@ namespace App\Security\Action;
 
 use App\Security\Entity\User;
 use App\Security\Message\Registration;
+use Nelmio\ApiDocBundle\Annotation as Nelmio;
+use OpenApi\Attributes as OpenApi;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -13,10 +15,49 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Messenger\HandleTrait;
 use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Validator\Exception\ValidationFailedException;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
+#[OpenApi\RequestBody(
+    description: 'Send user\'s information like email and plain password.',
+    content: new OpenApi\JsonContent(ref: new Nelmio\Model(type: Registration::class)),
+)]
+#[OpenApi\Response(
+    response: Response::HTTP_CREATED,
+    description: 'Returns the newly registered user.',
+    content: new OpenApi\JsonContent(ref: new Nelmio\Model(type: User::class, groups: ['get'])),
+)]
+#[OpenApi\Response(
+    response: Response::HTTP_UNPROCESSABLE_ENTITY,
+    description: 'Returns violation constraint list.',
+    content: new OpenApi\JsonContent(
+        type: 'object',
+        properties: [
+            new OpenApi\Property(
+                property: 'error',
+                type: 'object',
+                properties: [
+                    new OpenApi\Property(
+                        property: 'errors',
+                        type: 'array',
+                        items: new OpenApi\Items(
+                            type: 'object',
+                            properties: [
+                                new OpenApi\Property(type: 'string', property: 'propertyPath'),
+                                new OpenApi\Property(type: 'string', property: 'message'),
+                            ]
+                        )
+                    ),
+                ]
+            ),
+            new OpenApi\Property(property: 'code', type: 'integer'),
+            new OpenApi\Property(property: 'message', type: 'string'),
+        ]
+    ),
+)]
+#[OpenApi\Tag('Security')]
 #[Route('/register', name: 'register', methods: [Request::METHOD_POST])]
 final class Register extends AbstractController
 {
@@ -43,6 +84,11 @@ final class Register extends AbstractController
         /** @var User $user */
         $user = $this->handle($registration);
 
-        return $this->json($user, Response::HTTP_CREATED);
+        return new JsonResponse(
+            $serializer->serialize($user, 'json', [ObjectNormalizer::GROUPS => ['get']]),
+            Response::HTTP_CREATED,
+            ['Content-Type' => 'application/json'],
+            true
+        );
     }
 }
