4ee1a895f8ee2a52effed0f9f3a1abef4d89d4fa
TBoileau <t-boileau@email.com>
2022-01-28 10:18:23 +0100

    build: install nelmio/api-doc-bundle

--- server/config/bundles.php
+++ server/config/bundles.php
@@ -13,4 +13,5 @@ return [
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Gesdinet\JWTRefreshTokenBundle\GesdinetJWTRefreshTokenBundle::class => ['all' => true],
+    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
 ];
