c63ca8d43ba493914193fba3244cd45a890a4d51
TBoileau <t-boileau@email.com>
2022-02-02 17:00:52 +0100

    build: install api platform

--- server/config/bundles.php
+++ server/config/bundles.php
@@ -15,4 +15,6 @@ return [
     Gesdinet\JWTRefreshTokenBundle\GesdinetJWTRefreshTokenBundle::class => ['all' => true],
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
 ];
