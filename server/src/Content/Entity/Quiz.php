db3edfc24363fb8e5c3bb2a455b9bf464d41f976
TBoileau <t-boileau@email.com>
2022-02-06 12:46:42 +0100

    feat (content): get quiz (#200)

--- server/src/Content/Entity/Quiz.php
+++ server/src/Content/Entity/Quiz.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Content\Entity;
 
+use ApiPlatform\Core\Annotation\ApiResource;
 use App\Content\Doctrine\Repository\QuizRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
@@ -11,6 +12,12 @@ use Doctrine\ORM\Mapping\Entity;
 use Doctrine\ORM\Mapping\OneToMany;
 use Symfony\Component\Serializer\Annotation\Groups;
 
+#[ApiResource(
+    collectionOperations: [],
+    itemOperations: ['get'],
+    normalizationContext: ['groups' => ['read']],
+    routePrefix: '/content',
+)]
 #[Entity(repositoryClass: QuizRepository::class)]
 class Quiz extends Node
 {
