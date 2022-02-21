f6a80ff23d911cf1df10e8b57e9ee03174d55569
TBoileau <t-boileau@email.com>
2022-02-21 11:58:37 +0100

    refactor: guards

---
+++ client/src/app/security/guard/auth.guard.ts
@@ -0,0 +1,24 @@
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot} from "@angular/router";
+import {Inject, Injectable} from "@angular/core";
+import {SESSION_TOKEN, SessionInterface} from "../contracts/session";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  constructor(private router: Router, @Inject(SESSION_TOKEN) private session: SessionInterface) {
+  }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    if (this.session.authenticated()) {
+      return true;
+    }
+
+    this.router.navigate(['/login'], {
+      queryParams: {
+        return: state.url
+      }
+    });
+    return false;
+  }
+}
