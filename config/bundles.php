83eed7b48b2ad4d1eb6472472aac329e50daefda
Thomas Boileau <t-boileau@email.com>
2022-09-03 23:07:03 +0200

    Connexion (#309)
    * build: installation du symfony/translation
    
    * build: installation du symfony/web-profiler
    
    * feat (security): connexion (#298)

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
