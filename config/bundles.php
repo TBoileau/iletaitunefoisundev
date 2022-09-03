07010897f22d0e46503e4e98d930d95f5cd1a594
TBoileau <t-boileau@email.com>
2022-09-03 19:07:08 +0200

    build: installation de dépendances
    * symfony/doctrine-messenger
    * symfony/maker-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -7,4 +7,5 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
