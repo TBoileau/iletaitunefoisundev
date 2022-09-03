78483a3523ede2f5416ef54007eb7f86284c26c6
TBoileau <t-boileau@email.com>
2022-09-03 17:38:09 +0200

    build: installation de dépendances
    * symfony/password-hasher
    * symfony/security-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -4,4 +4,5 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
 ];
