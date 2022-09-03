35cc05330b3924f48bfed693b485e4e39b422677
TBoileau <t-boileau@email.com>
2022-09-03 17:14:30 +0200

    build: changement de l'autoload et installation de dépendances (#300)
    * App => IncentiveFactory\IlEtaitUneFoisUnDev
    * symfony/orm-pack
    * symfony/messenger

--- config/bundles.php
+++ config/bundles.php
@@ -2,4 +2,6 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
--- src/Kernel.php
+++ src/Kernel.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace IncentiveFactory\IlEtaitUneFoisUnDev;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
