2cca8b060597137ad83454b5786e83ac7b6ad16f
TBoileau <t-boileau@email.com>
2022-09-03 17:39:23 +0200

    build: installation du twig-pack

--- config/bundles.php
+++ config/bundles.php
@@ -5,4 +5,6 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
 ];
